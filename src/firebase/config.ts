9643dfcec1ad336544ed4d8d270875fbeabb3a21
Staygreen Gibilisco <mail.staygreen@gmail.com>
2025-12-04 14:59:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4855569420-fe978",
+  "appId": "1:285300522290:web:4572da7c3215439fa82b2b",
+  "apiKey": "AIzaSyAiRVDzFdMJMzLVDgcuSxk0SgI9ZInal3o",
+  "authDomain": "studio-4855569420-fe978.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "285300522290"
+};
