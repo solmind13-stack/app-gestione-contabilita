86043e1529160dc2b9d872e08f54de2fd982d78e
Staygreen Gibilisco <mail.staygreen@gmail.com>
2025-12-04 15:18:37 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,11 +21,13 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Source+Code+Pro:wght@400;500&display=swap" rel="stylesheet" />
       </head>
-      <body className={cn("font-body antialiased", "min-h-screen bg-background font-sans")}>
-        <FirebaseClientProvider>
-          {children}
-        </FirebaseClientProvider>
-        <Toaster />
+      <body>
+        <div className={cn("font-body antialiased", "min-h-screen bg-background font-sans")}>
+            <FirebaseClientProvider>
+            {children}
+            </FirebaseClientProvider>
+            <Toaster />
+        </div>
       </body>
     </html>
   );
