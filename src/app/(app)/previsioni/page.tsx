3f2ecf7ebf0c921df15bb407807cf7533e17239c
Staygreen Gibilisco <mail.staygreen@gmail.com>
2026-01-02 23:19:36 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/(app)/previsioni/page.tsx
+++ src/app/(app)/previsioni/page.tsx
@@ -169,7 +169,7 @@ export default function PrevisioniPage() {
         await deleteDoc(doc(firestore, collectionName, forecastId));
         toast({ title: toastTitle, description: `L'elemento è stato eliminato.` });
     } catch (e) {
-        toast({ variant: 'destructive', title: 'Errore', description: 'Impossibile eliminare l'elemento.' });
+        toast({ variant: 'destructive', title: 'Errore', description: "Impossibile eliminare l'elemento." });
         console.error(e);
     }
   };
